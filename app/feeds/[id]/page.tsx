7f5f8c420d1692e87a2d27abba504e7a06e1914e
genru <genjuroa@ymail.com>
2024-03-02 00:17:43 +0800

    Update page.tsx

--- app/feeds/[id]/page.tsx
+++ app/feeds/[id]/page.tsx
@@ -2,6 +2,8 @@ import { findFeed } from "@/utils/supabase/admin";
 import { createClient } from '@/utils/supabase/server';
 import { redirect } from "next/navigation";
 import Image from 'next/image'
+import { Play, Shuffle } from "lucide-react";
+import Card from "@/components/ui/Feed/Card";
 
 export default async function Feed({params}:{params:{id:string}}) {
 
@@ -35,26 +37,27 @@ export default async function Feed({params}:{params:{id:string}}) {
     }
     return (
         <section className="container relative m-auto flex overflow-hidden h-full">
-            <div className="w-96 h-full flex-shrink-0 overflow-y-auto overflow-x-hidden border border-yellow-600 transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidenav">
+            <div className="w-96 h-full flex-shrink-0 overflow-y-auto overflow-x-hidden transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidenav">
 
-                <div className="d-card d-card-compact w-96 bg-base-100 shadow-xl">
+                <div className="d-card d-card-compact w-96 bg-base-300 shadow-md rounded-2xl from-slate-800">
                     <figure>
                         <img src={feed.cover || 'https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg'} alt="Shoes" />
                         {/* <Image src={feed.cover || 'https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg'} alt={feed.title}  width={96} height={96}/> */}
                     </figure>
-                    <div className="d-card-body">
+                    <div className="d-card-body bg-transparent">
                         <h2 className="d-card-title">{feed.title}!</h2>
-                        <p>{feed.author}</p>
-                        <div className="d-card-actions justify-start">
-                        <button className="d-btn d-btn-sm d-btn-wide d-btn-primary">Play</button>
-                        <button className="d-btn d-btn-sm d-btn-wide d-btn-secondary">Play</button>
+                        <p className="text-xs">{feed.author}</p>
+                        <div className="d-card-actions justify-center flex flex-row">
+                            <button className="d-btn d-btn-sm d-btn-primary flex-grow-1 w-2/5 text-zinc-700 hover:text-white"> <Play width={16} height={16}></Play> Play</button>
+                            <button className="d-btn d-btn-sm d-btn-secondary flex-grow-1 w-2/5 text-zinc-700 hover:text-gray-200"> <Shuffle width={16} height={16}></Shuffle> Shuffle</button>
                         </div>
                         <p>{feed.description}</p>
                     </div>
                 </div>
             </div>
 
-            <div className="flex flex-col items-center flex-grow-1 gap-2 w-full border border-red-500">
+            <div className="flex flex-col items-start flex-grow-1 p-12 gap-2 w-full overflow-y-scroll border border-red-500">
+                <Card title="Card title!" description="Card description2" />
                 <div className="flex flex-col flow-grow-1 owerflow-auto">
                     <div className="d-card w-96 bg-base-100 shadow-xl">
                     <div className="d-card-body">
@@ -66,6 +69,7 @@ export default async function Feed({params}:{params:{id:string}}) {
                     </div>
                     </div>
                 </div>
+
                 <div className="flex flex-col flow-grow-1 owerflow-auto">
                     <div className="d-card w-96 bg-base-100 shadow-xl">
                     <div className="d-card-body">
