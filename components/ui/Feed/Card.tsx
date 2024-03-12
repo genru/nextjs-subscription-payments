21c127372ea4af5c153d2e01832b960cc0369ed3
genru <genjuroa@ymail.com>
2024-03-12 23:10:20 +0800

    feat: delete ui

--- components/ui/Feed/Card.tsx
+++ components/ui/Feed/Card.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { Pause, Play, RefreshCw, AlertTriangle } from "lucide-react";
+import { Pause, Play, RefreshCw, AlertTriangle, Trash2 } from "lucide-react";
 import AudioComponent from "./AudioComponent";
 import { SyntheticEvent, useRef, useState } from "react";
 import { cn } from "@/utils/cn";
@@ -49,13 +49,14 @@ export default function Card({...props}) {
     let badMedia = !props.audioSrc;
     return (
         <div className="flex flex-col min-w-full flow-grow-1 owerflow-auto my-0">
-            <div className="d-card d-card-compact shadow-0 border-base-300 bg-base-100 hover:bg-base-200">
+            <div className="d-card d-card-compact shadow-0 border-base-300 bg-base-100 group/item hover:bg-base-200">
             {/* <figure className=""><img src={props.cover} alt="Shoes" /></figure> */}
                 <div className="d-card-body">
                     <div className="flex">
                         <figure className="w-20 rounded-sm ring ring-neutral ring-offset-base-100 ring-offset-2 mr-4"><img src={props.cover} alt="Album"/></figure>
-                        <div className="flex flex-col">
-                            <h2 className="d-card-title text-base font-normal">{props.title}</h2>
+                        <div className="flex flex-col w-full">
+                            {/* <h2 className="d-card-title text-base font-normal">{props.title}</h2> */}
+                            <NavbarAction title={props.title}/>
                             <p className="text-sm mt-1">{props.description}</p>
                         </div>
                     </div>
@@ -96,5 +97,32 @@ function ProgressBar({...props}) {
             <div className="bg-accent h-1 rounded-lg absolute top-1 pointer-events-none" style={{ width: `${props.currentTime * 100 / props.duration}%` }} ></div>
         </div>
 
+    )
+}
+
+function NavbarAction({...props}) {
+    return (
+        <div className="d-navbar h-4 min-h-8 p-0 ">
+            <div className="flex-1">
+                <h2 className="text-lg">{props.title}</h2>
+            </div>
+            <div className="flex-none">
+            <ul className="d-menu d-menu-horizontal d-rounded-box invisible group-hover/item:visible">
+                <li className="d-tooltip" data-tip="delete this episode?">
+                    <button>
+                        <Trash2 className="text-error" width={18} height={18}/>
+                    </button>
+                </li>
+            </ul>
+                {/* <div className="d-dropdown d-dropdown-end">
+                    <div tabIndex={0} role="button" className="d-btn  d-btn-xs d-btn-square d-btn-ghost m-1">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
+                    </div>
+                    <ul tabIndex={0} className="d-dropdown-content z-[1] d-menu p-2 shadow bg-base-100 rounded-box w-32">
+                        {props.menuItems}
+                    </ul>
+                </div> */}
+            </div>
+        </div>
     )
 }
